824678f35974021cf206f1c6b2df7511b6afc4e4
Yuki-Kurokawa <yuki.kurokawa.0705@gmail.com>
2020-02-17 23:40:43 +0900

    make userview test

--- chat/tests/test_views.py
+++ chat/tests/test_views.py
@@ -18,9 +18,8 @@ class UserViewTest(TestCase):
                                 }
                                )
         self.assertEqual(response.status_code, 200)
-        created_model = User.objects.filter(username = "create_test")
-        self.assertEqual(created_model[0].username,"create_test")
-
+        created_model = User.objects.filter(username="create_test")
+        self.assertEqual(created_model[0].username, "create_test")
 
     def test_signin(self):
         client = Client()
@@ -39,23 +38,24 @@ class UserViewTest(TestCase):
         response = client.get("/chat/signout/")
         self.assertEqual(response.status_code, 200)
 
+
 class LobbyViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         User.objects.create_user(username="test",
                                  password="test").save()
-    
+
     def test_lobby_top_login(self):
         client = Client()
         client.login(username="test",
                      password="test"
                      )
         response = client.get("/chat/lobby/")
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
 
     def test_lobby_top_not_login(self):
         client = Client()
-        response = client.get("/chat/lobby/",follow=True)
-        self.assertEqual(response.redirect_chain[0][0],"chat/signin?next=/chat/lobby/")
-    
-    
+        response = client.get("/chat/lobby/", follow=True)
+        self.assertEqual(
+            response.redirect_chain[0][0],
+            "chat/signin?next=/chat/lobby/")
